94e13c25f9ffbaf1e78511793c7b408036272647
EdgarStroh <edgarstroh@googlemail.com>
2025-02-16 14:36:15 +0100

    made startscreen

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+// import { routes } from './app.routes';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+
+const routes = [
+  { path: '', component: StartScreenComponent },
+  
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
