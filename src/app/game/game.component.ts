94686ae4f296d789eb8226c2d0409edda5e4aa0a
EdgarStroh <edgarstroh@googlemail.com>
2025-02-17 17:00:14 +0100

    added game logic and animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importieren des CommonModules
+import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule], // Hinzufügen von CommonModule
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
   cards = [0, 1, 2, 3, 4];
+  pickCardAnimation = false;
+  game: Game = new Game();
+  currentCard: string = '';
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop() || '';
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.game.playerCards.push(this.currentCard)
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,29 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playerCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
+        }
+        this.shuffle(this.stack);
+    }
+
+    private shuffle(array: string[]) {  // <-- Methode innerhalb der Klasse
+        let currentIndex = array.length;
+
+        while (currentIndex != 0) {
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]
+            ];
+        }
+    }
+}
