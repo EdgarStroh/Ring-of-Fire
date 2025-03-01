21a704120c32f2ccdc5d2b58ca7d86701cbd93f2
EdgarStroh <edgarstroh@googlemail.com>
2025-03-01 15:06:11 +0100

    added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-// import { routes } from './app.routes';
+// 🔹 Deine Routen
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { GameComponent } from './game/game.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+// 🔥 Firebase-Module importieren
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+// 🔥 Firebase-Konfiguration importieren
+import { environment } from '../environments/environment';
 
 const routes = [
   { path: '', component: StartScreenComponent },
@@ -12,5 +20,13 @@ const routes = [
 ];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+
+    // 🔥 Firebase-Integration
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCLsvGvMvt7n_KHg8YUhY4hiA-fn4A1xNA",
+        authDomain: "ring-of-fire-2cd78.firebaseapp.com",
+        projectId: "ring-of-fire-2cd78",
+        storageBucket: "ring-of-fire-2cd78.firebasestorage.app",
+        messagingSenderId: "883574471014",
+        appId: "1:883574471014:web:ce9d898caf0a788155b0a7"
+    }
+  };
+  
